f13f561d620d761663651b0dbdd98b4af08760c1
Paweł <xAranaktu@pm.me>
2023-02-05 10:35:18 +0100

    Add files via upload

---
+++ lua/scripts/generate_minifaces_user_team.lua
@@ -0,0 +1,39 @@
+-- Generate miniface for everyplayer in the user team
+-- May take around 5 mins to complete, so be patient.
+
+-- Execute only if we are in career mode
+if not IsInCM() then return end
+
+-- Path is relative to the game installation directory
+PlayerCaptureSetOutputDirectory("LiveEditorMods\\root\\Legacy\\data\\ui\\imgAssets\\heads")
+
+-- 0 - Head and shoulders
+-- 1 - Head
+-- 2 - Body
+PlayerCaptureSetCamera(1)
+
+-- 256x256
+PlayerCaptureSetSize(256, 256)
+
+-- Image Type
+-- 0 - PNG
+-- 1 - DDS
+PlayerCaptureSetType(1)
+
+local user_teamid = GetUserTeamID()
+
+-- Get all rows for teamplayerlinks table
+rows = GetDBTableRows("teamplayerlinks")
+
+for i=1, #rows do
+    local tplink = rows[i]
+
+    local iteamid = math.floor(tplink.teamid.value)
+    if user_teamid == iteamid then
+        local iplayerid = math.floor(tplink.playerid.value)
+        PlayerCaptureAddPlayer(iplayerid, iteamid)
+    end
+end
+
+-- Start Capturing
+PlayerCaptureStart()
